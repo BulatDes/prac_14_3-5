90d1f6dd744bd6b9ed35f6fa38620a5800287ae3
BulatDes <123715505+BulatDes@users.noreply.github.com>
2023-05-03 10:17:06 +0500

    Add files via upload

---
+++ Prac_14_3-5/Prac_14_3-5/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿
+namespace Prac_14_3_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Output = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.zadanie4 = new System.Windows.Forms.Button();
+            this.zadanie5 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите размер очереди";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(52, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 2;
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(12, 84);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(99, 23);
+            this.Output.TabIndex = 3;
+            this.Output.Text = "Вывод очереди";
+            this.Output.UseVisualStyleBackColor = true;
+            this.Output.Click += new System.EventHandler(this.Output_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Output);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(0, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(278, 134);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Задание 3";
+            // 
+            // zadanie4
+            // 
+            this.zadanie4.Location = new System.Drawing.Point(12, 168);
+            this.zadanie4.Name = "zadanie4";
+            this.zadanie4.Size = new System.Drawing.Size(99, 22);
+            this.zadanie4.TabIndex = 6;
+            this.zadanie4.Text = "Вывод";
+            this.zadanie4.UseVisualStyleBackColor = true;
+            this.zadanie4.Click += new System.EventHandler(this.zadanie4_Click);
+            // 
+            // zadanie5
+            // 
+            this.zadanie5.Location = new System.Drawing.Point(396, 16);
+            this.zadanie5.Name = "zadanie5";
+            this.zadanie5.Size = new System.Drawing.Size(75, 23);
+            this.zadanie5.TabIndex = 7;
+            this.zadanie5.Text = "Вывод";
+            this.zadanie5.UseVisualStyleBackColor = true;
+            this.zadanie5.Click += new System.EventHandler(this.zadanie5_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 202);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(191, 134);
+            this.listBox1.TabIndex = 9;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(396, 43);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(191, 108);
+            this.listBox2.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.zadanie5);
+            this.Controls.Add(this.zadanie4);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Output;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button zadanie4;
+        private System.Windows.Forms.Button zadanie5;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ Prac_14_3-5/Prac_14_3-5/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.IO;
+using static System.Windows.Forms.AxHost;
+
+namespace Prac_14_3_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Output_Click(object sender, EventArgs e)
+        {
+            label2.Text = "";
+            bool flag = true;
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Заполните поле ввода данных");
+                flag = false;
+            }
+            else
+            {
+                for (int i = 0; i < textBox1.Text.Length; i++)
+                {
+                    if (!Char.IsDigit(textBox1.Text[i]))
+                    {
+                        MessageBox.Show("Введите только числа");
+                        flag = false;
+                    }
+                }
+            }
+            if (flag == true)
+            {
+                string num = textBox1.Text;
+                int n = int.Parse(num);
+                Queue<int> queue = new Queue<int>();
+                for (int i = 1; i <= n; i++)
+                {
+                    queue.Enqueue(i);
+                }
+                foreach (var count in queue)
+                {
+                    label2.Text = label2.Text + $"{count} ";
+                }
+            }
+        }
+
+        private void zadanie4_Click(object sender, EventArgs e)
+        {
+            Queue molodye = new Queue();
+            Queue starye = new Queue();
+            if (File.Exists("text.txt"))
+            {
+                listBox1.Items.Clear();
+
+                StreamReader sr = File.OpenText("text.txt");
+                while (!sr.EndOfStream)
+                {
+                    string s = sr.ReadLine();
+                    string[] ss = s.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    string fam = ss[0];
+                    string name = ss[1];
+                    string otch = ss[2];
+                    int old = int.Parse(ss[3]);
+                    double ves = double.Parse(ss[4]);
+                    if (old < 40)
+                    {
+                        molodye.Enqueue($"{fam} {name} {otch} {old} {ves}");
+                    }
+                    else
+                    {
+                        starye.Enqueue($"{fam} {name} {otch} {old} {ves}");
+                    }
+                }
+                sr.Close();
+                foreach (string pers in molodye)
+                {
+                    listBox1.Items.Add(pers);
+                }
+                foreach (string pers in starye)
+                {
+                    listBox1.Items.Add(pers);
+                }
+
+            }
+
+        }
+
+        private void zadanie5_Click(object sender, EventArgs e)
+        {
+            Queue people = new Queue();
+            if (File.Exists("text.txt"))
+            {
+                listBox2.Items.Clear();
+
+                StreamReader sr = File.OpenText("text.txt");
+                while (!sr.EndOfStream)
+                {
+                   people.Enqueue(sr.ReadLine());
+                }
+                sr.Close();
+                people = new Queue<string>(people.OrderBy(p => int.Parse(p.Split()[3])));
+                Queue sortedPeople = new Queue();
+
+                foreach (string person in people)
+                {
+                    string[] parts = person.Split();
+                    sortedPeople.Enqueue($"{parts[0]} {parts[1]} {parts[2]}, возраст: {parts[3]}, вес: {parts[4]}");
+                }
+                foreach (string person in sortedPeople) { listBox2.Items.Add(person); }
+
+            }
+        }
+    }
+}
